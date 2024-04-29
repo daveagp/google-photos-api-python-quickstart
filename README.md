30d83e2b9292f3c588f88198e4545fe91b69d2a8
David Pritchard <daveagp@gmail.com>
2024-04-29 09:43:36 -0700

    Fold in my v0 changes

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-# google-photos-api-python-quickstart
-Quickstart example of how to use Google Photos API with Python
+# Use Google Photos API and Python to download or explore your photo collection
 
-This example show how to use Python to access Google Photos API v1 to list 10 latest albums.
+(Forked from another quickstart guide, kudos to ido-ran)
+
+This repo allows you to
+
+- run a Python program on your computer that connects to your Google Photos account using the Google Photos API
+- and then download metadata and photos to your local machine
+
+For example, this can be used to create an offline backup, run a screensaver on your machine, or to export your photos if you want to stop using Google Photos.
+
+## HOWTO
+
+1. You have to create a "Google APIs & Services" application. This is using the Google Cloud Console. You don't actually need to sign up or pay for Google Cloud services itself.
+
+2. On the "credentials" tab, create a Web application client id. Download its client secret file as json, and take the `client_secret.json` and put it in the same directory as this repo. It will contain something like `{"web":{"client_id":"61.....apps.googleusercontent.com","project_id":"name-you-chose","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"..."}}`
+
+3. Under "Authorized redirect URIs" for your Web application, add `http://localhost:8888/`
+
+4. On the "OAuth consent screen" tab, leave your application in testing mode (no need to "PUBLISH APP"), but add your personal gmail account as a test user.
+
+5. The first time you use it, you'll have to use a brower to approve the app's access to your account. The Python script will output the URL you need to visit.
+
+## APPLICATIONS
+
+To create a slideshow of your photos, here is a nice program, and a way to caption the photos by their exif date:
+
+`feh "/path/to/your/images/" --info "exiftool -DateTimeOriginal %F | cut -d : -f 2- | head -c 11 | sed s/:/-/g" -ZxFz -D 5`
+
+## TROUBLESHOOTING
+
+If you get an `invalid_grant: Token has been expired or revoked` error, try deleting the `token.pickle` file.
+
+## TODOs
+
+* Make `feh` into an actual screensaver that turns on when your computer detects no user activity.
+
+* Check for duplicate photos (e.g. with different size) by looking for duplicate exif timestamps or filenames.
--- quickstart.py
+++ quickstart.py
@@ -1,8 +1,8 @@
 """
-Shows basic usage of the Photos v1 API.
+Use the Google Photos API to download your photos
 
-Creates a Photos v1 API service and prints the names and ids of the last 10 albums
-the user has access to.
+You can filter on different conditions. Use Python exif libraries to make sure
+the exported photos contain dates that are more or less consistent with Google.
 """
 from __future__ import print_function
 import os 
@@ -12,6 +12,12 @@ from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 import google_auth_httplib2  # This gotta be installed for build() to work
+import requests # edited
+from PIL import Image, ExifTags
+from datetime import datetime
+from datetime import timedelta
+from dateutil import parser
+import piexif
 
 # Setup the Photo v1 API
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
@@ -24,18 +30,107 @@ if not creds or not creds.valid:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
-        creds = flow.run_local_server(port = 0)
+        creds = flow.run_local_server(port = 8888)
     with open("token.pickle", "wb") as tokenFile:
         pickle.dump(creds, tokenFile)
 service = build('photoslibrary', 'v1', credentials = creds, static_discovery = False)
 
 # Call the Photo v1 API
-results = service.albums().list(
-    pageSize=10, fields="nextPageToken,albums(id,title)").execute()
-items = results.get('albums', [])
-if not items:
-    print('No albums found.')
-else:
-    print('Albums:')
+PAGE_SIZE = 100
+results = service.mediaItems().list(
+    pageSize=PAGE_SIZE, fields="nextPageToken,mediaItems").execute()
+items = results.get('mediaItems', [])
+token = results.get('nextPageToken', None)
+
+INFO_DIR = "/home/daveagp/pyphotos/info/"
+IMAGE_DIR = "/home/daveagp/pyphotos/images/"
+MAX_DOWNLOADS = 50000
+MAX_INFOS = 100000
+
+downloads = 0
+infos = 0
+
+while token is not None and infos < MAX_INFOS:
     for item in items:
-        print('{0} ({1})'.format(item['title'].encode('utf8'), item['id']))
+        info_filename = INFO_DIR + item['id']
+        googleTimestamp = None
+        if os.path.isfile(info_filename):
+            print("Already done", item['id'], "skipping")
+            continue
+        else:
+            print()
+            print('id:', item['id'])
+            if 'mediaMetadata' in item and 'creationTime' in item['mediaMetadata']:
+                googleTimestamp = item['mediaMetadata']['creationTime']
+                print(googleTimestamp)
+            infos += 1
+            json.dump(item, open(info_filename, "w"))
+        if item['mimeType'] != 'image/jpeg':
+            print('Skipping mimeType', item['mimeType'])
+            continue
+        if googleTimestamp[:10] >= "2015-05-14":
+            print('Skipping recent', googleTimestamp[:10])
+            continue
+        if downloads >= MAX_DOWNLOADS:
+            continue
+        image_filename = IMAGE_DIR + item['id'] + '.jpg'
+        if not os.path.isfile(image_filename):
+            r = requests.get(item['baseUrl']+"=w1920-h1080")#"=d")
+            if r.status_code == 200:
+                open(image_filename, "wb").write(r.content)
+                downloads += 1
+                PILobj = Image.open(image_filename)
+                exif = {
+                    ExifTags.TAGS[k]: v
+                    for k, v in PILobj._getexif().items()
+                    if k in ExifTags.TAGS
+                }
+                exifDate = None
+                for k, v in exif.items():
+                    if 'DateTimeOriginal' in k:
+                        print(k, v)
+                        # ignore invalid dates we sometimes see
+                        if v[:4] != '0000':
+                            exifDate = v
+                needsNewExifDate = exifDate is None
+                googleDate = parser.isoparse(googleTimestamp)
+                print(googleDate)
+                if exifDate is not None:
+                    exif_dt = datetime.strptime(exifDate, "%Y:%m:%d %H:%M:%S")
+                    days_diff = (exif_dt - googleDate.replace(tzinfo=None)).days
+                    if abs(days_diff) > 2:
+                        print('needs new exif date')
+                        print(exifDate)
+                        print(googleTimestamp)
+                        needsNewExifDate = True
+                if needsNewExifDate:
+                    exif_dict = piexif.load(image_filename)
+                    # this seems to be hard-coded due to my account? or else there is not enough info exposed by google photos api to do better
+                    new_date = (googleDate - timedelta(hours=7)).strftime("%Y:%m:%d %H:%M:%S")
+                    print(new_date)
+                    exif_dict['0th'][piexif.ImageIFD.DateTime] = new_date
+                    exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = new_date
+                    exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = new_date
+                    exif_bytes = piexif.dump(exif_dict)
+                    piexif.remove(image_filename)
+                    piexif.insert(exif_bytes, image_filename)
+            else:
+                print('Status', r.status_code)
+
+    results = service.mediaItems().list(
+        pageSize=PAGE_SIZE,
+        pageToken=token,
+        fields="nextPageToken,mediaItems").execute()
+    items = results.get('mediaItems', [])
+    token = results.get('nextPageToken', None)
+
+"""- creationTime is meant to reference the camera's zone when the photo was taken
+- the time zone is not explicitly part of the Google Photos API, but it is there implicitly
+-- e.g. Toronto photos have UTC in creationTime but Toronto time in Photos UI, and these can be different dates
+- EXIF generally has the right time/date/zone
+
+therefore
+
+- use EXIF when it's available. Seemingly it will match Photos UI
+- otherwise inject Google Photos API creationTime into EXIF DateTimeOriginal. We can't know the time zone, so leave out the time zone
+"""
